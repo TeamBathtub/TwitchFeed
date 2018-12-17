f79bc7c644a522ed4a76543cb1812e361dbf9619
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-17 14:18:39 -0800

    working on search functionality

---
+++ app/src/components/streamers/Streamers.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="streamers">
+    <h2>Search by Streamer Name</h2>
+
+    <StreamerSearch v-bind:onSearch="handleSearch" v-bind:search="search"/>
+    
+    <div class="search-container">
+      <ul v-if="streamers">
+        <Streamer v-for="(streamer, index) in streamers"
+          v-bind:key="index"
+          v-bind:streamer="streamer"/>
+      </ul>
+    </div>
+
+  </section>
+</template>
+
+<script>
+import api from '../../services/api.js';
+import Streamer from './Streamer.vue';
+import StreamerSearch from './StreamerSearch.vue';
+
+export default {
+  data() {
+    const search = this.$route.query.search;
+    return {
+      streamers: null,
+      filteredStreamers: [],
+      search: search ? decodeURIComponent(this.$route.query.search) : ''
+    };
+  },
+  components: {
+    Streamer,
+    StreamerSearch
+  },
+  created() {
+    this.searchStreamers();
+  },
+  watch: {
+    $route(newRoute) {
+      const newSearch = newRoute.query.search;
+      this.search = decodeURIComponent(newSearch);
+      this.searchStreamers();
+    }
+  },
+  methods: {
+    handleSearch(search) {
+      this.search = search || '';
+      this.searchStreamers();
+    },
+    searchStreamers() {
+      api.getStreamers(this.search)
+        .then(response => {
+          this.streamers = response.user_name;
+        })
+        .catch(err => {
+          this.error = err.message;
+          this.people = null;
+        });
+    }
+  }
+};
+</script>
--- app/src/services/api.js
+++ app/src/services/api.js
@@ -2,5 +2,17 @@ export default {
   test() {
     return fetch('/api')
       .then(response => response.json());
+  },
+
+  getStreamers(searchTerm = '') {
+    return fetch(`https://api.twitch.tv/helix/users?login=${encodeURIComponent(searchTerm)}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Client-ID': '8sb2kt99biht5q3k79k7hsejyj0q2y'
+      }
+    })
+      .then(response => response.json());
+
   }
 };
