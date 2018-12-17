b91bb47cbe0f3307c20e51ffabd8a603cb1753ba
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-17 11:57:11 -0800

    got table create, load, drop files working- able to see profile table with seed data in psql

--- server/scripts/create-tables.js
+++ server/scripts/create-tables.js
@@ -1,12 +1,14 @@
-const client = require('../lib/db-client');
 require('dotenv').config();
+const client = require('../lib/db-client');
 
 
 client.query(`
 CREATE TABLE IF NOT EXISTS profile (
- id SERIAL PRIMARY KEY,
- username VARCHAR(256) NOT NULL,
- hash VARCHAR(256) NOT NULL
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(256) NOT NULL,
+  first_name VARCHAR(256),
+  email VARCHAR(256),
+  hash VARCHAR(256) NOT NULL
 );
 `)
   .then(
