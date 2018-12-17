47fafa741c1d6be26e821c57e43576f7a4969ea0
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-17 14:47:35 -0800

    added in routerlinks

--- app/src/components/home/Home.vue
+++ app/src/components/home/Home.vue
@@ -1,3 +1,10 @@
 <template>
- <h1> HELLO </h1>
+  <nav v-if="user">
+   <RouterLink to="/">Home</RouterLink>
+   <RouterLink to="/favorites">Favorites</RouterLink>
+   <RouterLink to="/streamers">Search</RouterLink>
+   <RouterLink to="/ratings">Ratings</RouterLink>
+   <RouterLink to="/about">About Us</RouterLink>
+   <a href="#" @click="handleLogout">Logout</a>
+ </nav>
 </template>
--- app/src/services/api.js
+++ app/src/services/api.js
@@ -57,4 +57,5 @@ export default {
       }
     })
       .then(response => response.json());
+  }
 };
