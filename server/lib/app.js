f74006eaafe776630a4e798d747ccffbf7b431bd
Marcy Silverman <marcysilve225@gmail.com>
2018-12-17 16:46:43 -0800

    token working

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,42 +1,41 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const twitch = require('./routes/twitch/twitch');
 const auth = require('./routes/auth/auth');
-// const twitch = require('./routes/twitch/twitch'); 
-// const jwt = require('./jwt');
+const twitch = require('./routes/twitch/twitch'); 
+const jwt = require('./jwt');
 
 // enhanced logging
 app.use(morgan('dev'));
 
+// register the json "middleware" body parser
 app.use(express.json());
 
 app.use(express.static('public'));
 
-app.use('/api/twitch', twitch);
 //don't forget to put checkAuth//
-// function checkAuth(req, res, next) {
-//   const token = req.get('Authorization');
-//   console.log('token\n\n', token); 
-//   if(!token) {
-//     res.status(401).json({ error: 'no authorization found' });
-//     return;
-//   }
+function checkAuth(req, res, next) {
+  const token = req.get('Authorization');
+  console.log('token\n\n', token); 
+  if(!token) {
+    res.status(401).json({ error: 'no authorization found' });
+    return;
+  }
 
-//   let payload = null;
-//   try {
-//     payload = jwt.verify(token); 
+  let payload = null;
+  try {
+    payload = jwt.verify(token); 
 
-//   }
-//   catch (err) {
-//     res.status(401).json({ error: 'invalid token' });
-//     return;
-//   }
-//   req.userId = payload.id;
-//   next(); 
-// }
+  }
+  catch (err) {
+    res.status(401).json({ error: 'invalid token' });
+    return;
+  }
+  req.userId = payload.id;
+  next(); 
+}
 
 app.use('/api/auth', auth);
-// app.use('/api/twitch', checkAuth, twitch);
+app.use('/api/twitch', checkAuth, twitch);
 
 module.exports = app;
