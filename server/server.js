3a4e2fc08644250aba021aebc5881ca642a14193
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-18 12:02:38 -0800

    pushing changes to twitch app

--- server/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const app = require('./lib/app');
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-  console.log('server app started on port', PORT);
-});
