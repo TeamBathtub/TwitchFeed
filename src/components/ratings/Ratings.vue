6aa290f2b82c9dbc937dd9ffe756908db8294f46
Tyler Corbett <thetylercorbett@gmail.com>
2018-12-18 14:28:56 -0800

    ratings and ratingdisplay started

--- src/components/ratings/Ratings.vue
+++ src/components/ratings/Ratings.vue
@@ -1,15 +1,64 @@
 <template>
   <section>
     <h1>I am Ratings</h1>
+    <button v-on:click="getRandomStreamer()">Random Stream</button>
+    <RatingsDisplay
+    v-if="randomName"
+    :randomName="randomName"
+    :randomThumbnail="randomThumbnail"
+    />
   </section>
 </template>
 
 <script>
+import api from '../../services/api';
+import RatingsDisplay from './RatingsDisplay';
 export default {
-
+  data() {
+    return {
+      streams: null,
+      labels: null,
+      randomName: null,
+      randomThumbnail: null
+    };
+  },
+  created() {
+    api.getTopStreamers()
+      .then(response => {
+        return this.streams = response;
+      })
+      .then(() => this.labels = this.getLabels());
+  },
+  methods: {
+    getLabels() {
+      return this.streams.map(stream => {
+        return stream.user_name;
+      });
+    },
+    getRandomStreamer() {
+      let index = Math.floor((Math.random() * this.streams.length) + 1);
+      this.randomName = this.labels[index];
+      this.getStreamerThumbnail();
+    },
+    getStreamerThumbnail() {
+      api.getStreamers(this.randomName)
+        .then(response => {
+          return this.randomThumbnail = response.data[0].profile_image_url;
+        })
+        .catch(err => {
+          this.error = err.message;
+        });
+    },
+  
+  },
+  components: {
+    RatingsDisplay
+  }
 };
 </script>
 
-<style>
-
+<style scoped>
+  
 </style>
+
+
---
+++ src/components/ratings/RatingsDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="randomStreamer">
+      <h2>{{ randomName }}</h2>
+      <img :src="randomThumbnail">
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    randomName: String,
+    randomThumbnail: String
+  },
+};
+</script>
+
+<style scoped>
+#randomStreamer {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    width: 100px;
+}
+img {
+    width: 100%;
+    height: 100%;
+}
+</style>
