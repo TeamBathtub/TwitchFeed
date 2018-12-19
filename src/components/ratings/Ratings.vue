6be0a4292989f410caa5235d66a2338fead0b124
Tyler Corbett <thetylercorbett@gmail.com>
2018-12-18 16:40:29 -0800

    removed comment

--- src/components/ratings/Ratings.vue
+++ src/components/ratings/Ratings.vue
@@ -20,7 +20,6 @@
 import api from '../../services/api';
 import RatingsDisplay from './RatingsDisplay';
 import RatingsForm from './RatingsForm';
-//test 2fdadsfs
 export default {
   data() {
     return {
