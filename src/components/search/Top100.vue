37ef9801eb3f74d31af06a005c2788abaceb38e4
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-19 16:08:19 -0800

    readded the css that got lost

--- src/components/search/Top100.vue
+++ src/components/search/Top100.vue
@@ -28,29 +28,10 @@ export default {
 ul {
   display: grid;
   list-style-type: none;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
   padding: 0;
   grid-column-gap: 10px;
   grid-row-gap: 10px;
 }
-li {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-evenly;
-  border: 2px solid darkgray;
-  box-shadow: 2px 2px 2px darkgray;
-  background: black;
-  margin: 5px 10px;
-  text-align: center;
-}
-h3 {
-  margin-bottom: 0px;
-}
-p, h3{
-  color: white;
-}
-button {
-  margin-bottom: 15px;
-}
+
 </style>
