55ea4fc7e4906ab6eb488d96f38d0e9b80af44a7
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-19 16:42:59 -0800

    pictures working with favorites list

---
+++ src/components/favorites/FavoriteDisplay.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+      {{favorite.userName}}
+      <a :href="url">View Stream</a>
+      <img :src="thumbnail"/>
+    <!-- <button @click="handleDelete"> Delete </button> -->
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    url: String,
+    thumbnail: String,
+    favorite: Object
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/components/favorites/FavoriteItem.vue
+++ src/components/favorites/FavoriteItem.vue
@@ -1,16 +1,45 @@
 <template>
   <section>
     <li>
-      {{favorite.userName}}
+      <FavoriteDisplay 
+        :favorite="favorite"
+        :thumbnail="thumbnail"
+        :url="url"
+      />
     </li>
     <!-- <button @click="handleDelete"> Delete </button> -->
   </section>
 </template>
 
 <script>
+import api from '../../services/api';
+import FavoriteDisplay from './FavoriteDisplay';
 export default {
   props: {
+    url: String,
+    thumbnail: String,
     favorite: Object
+  },
+  created() {
+    this.makeUrl();
+    this.getStreamerThumbnail();
+  },
+  methods: {
+    getStreamerThumbnail() {
+      api.getStreamers(this.favorite.userName)
+        .then(response => {
+          return this.thumbnail = response.data[0].profile_image_url;
+        })
+        .catch(err => {
+          this.error = err.message;
+        });
+    },
+    makeUrl() {
+      return this.url = 'https://www.twitch.tv/' + this.favorite.userName;
+    }
+  },
+  components: {
+    FavoriteDisplay
   }
 };
 </script>
@@ -18,3 +47,4 @@ export default {
 <style scoped>
 
 </style>
+
