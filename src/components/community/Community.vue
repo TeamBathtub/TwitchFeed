93535749aac6036874a041e789a1e89c1d98dd7c
Tyler Corbett <thetylercorbett@gmail.com>
2018-12-20 11:52:51 -0800

    table doesnt create duplicate heads

--- src/components/community/Community.vue
+++ src/components/community/Community.vue
@@ -1,12 +1,21 @@
 <template>
   <section> 
     <h1>Community</h1>
-    <h1>Community Rating of Streamers</h1>
-    <CommunityRating 
-      v-for="stat in stats"
-      :stat="stat"
-      :key="stat.user_name" />
-
+    <table>
+      <thead>
+       <tr>
+         <th>Streamer</th>
+         <th>Average</th>
+       </tr>
+     </thead>
+     <tbody>
+        <CommunityRating 
+          v-for="stat in stats"
+          :stat="stat"
+          :key="stat.user_name" 
+        />
+     </tbody>
+    </table>
     <h1>Community Favorites</h1>
     <CommunityFavorite 
     v-for="favorite in favorites"
@@ -51,5 +60,14 @@ export default {
 </script>
 
 <style>
-
+h1 {
+  text-align: center;
+  font-size: 6vh;
+  margin: 2vh 35vw 2vh 35vw;
+  border: solid gray 2px;
+  border-radius: 20vw;
+  color: gray;
+  width: 25vw;
+  justify-content: center;
+}
 </style>
--- src/components/community/CommunityRating.vue
+++ src/components/community/CommunityRating.vue
@@ -1,19 +1,11 @@
 <template>
 <section>
-  <table align="center" v-if="stat">
-     <thead>
-       <tr>
-         <th>Streamer</th>
-         <th>Average</th>
-       </tr>
-     </thead>
      <tbody>
        <tr>
          <td> {{stat.user_name}} </td>
          <td> {{stat.average}} </td>
        </tr>
      </tbody>
-    </table>
   </section>
 </template>
 
