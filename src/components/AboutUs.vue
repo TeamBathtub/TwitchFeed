7d96da4a741215eb86a96904fd8d22c98d022592
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-20 19:46:52 -0800

    name fixed with spelling

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -18,16 +18,16 @@
             <div class="text">Marcy Silverman</div>
           </div>
       </div>
-      <p>Budding Developer that loves naping, working out, eating, and petting dogs.</p>
+      <p>Budding Developer that loves napping, working out, eating, and petting dogs.</p>
     </div>
     <div class="person">
       <div class="container">
-        <img src="../../assets/kristen.jpg" alt="Avatar" class="image" style="width:100%">
+        <img src="../../assets/kristin.jpg" alt="Avatar" class="image" style="width:100%">
           <div class="middle">
-            <div class="text">Kristen Hortsch</div>
+            <div class="text">Kristin Hortsch</div>
           </div>
       </div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In tempore, reiciendis voluptas amet commodi impedit at corrupti esse nisi minus voluptates repellat vitae excepturi assumenda officia optio ipsam enim pariatur.</p>
+      <p>Avid laugher, sports fan, stalker of cats, and coding (Codewars) addict!</p>
     </div>
   </section>
 </template>
